a0a9618201f67bea15aa090cc4cde54ee2ce5aab
NavaneethKrishnan m <97788158+iam-navaneeth@users.noreply.github.com>
2023-07-01 09:52:57 +0530

    Update heart_disease_prediction_web_app.py

--- heart_disease_prediction_web_app.py
+++ heart_disease_prediction_web_app.py
@@ -12,7 +12,7 @@ import streamlit as st
 
 
 #loading the saved model
-loaded_model = pickle.load(open('D:/deploy_ml/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
 
 #creating a function for prediction 
