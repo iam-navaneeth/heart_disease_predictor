18040de5da1c3db0abb71b57fbb43e1f89c67d03
NavaneethKrishnan m <97788158+iam-navaneeth@users.noreply.github.com>
2023-07-01 10:16:07 +0530

    Update heart_disease_prediction_web_app.py

--- heart_disease_prediction_web_app.py
+++ heart_disease_prediction_web_app.py
@@ -42,8 +42,7 @@ def main():
     st.title('Heart disease prediction web app')
     st.write('*created by Navaneethkrishnan M*')
     st.write("[Buy me a coffee!!☕](https://www.buymeacoffee.com/navaneethnk)")
-    st.write("PS : This predictor have 85% accuracy")
-    st.write("Enter values based on the medical reports")
+    st.write("PS : This predictor have 85% accuracy,Enter values based on the medical reports ")
 
     
     #getting the data from user
